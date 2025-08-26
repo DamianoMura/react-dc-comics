7f6476627754bb00cfc85de592ae56aac77dfc77
DamianoMura <mura.damiano14@gmail.com>
2025-08-26 23:40:21 +0200

    [upd] included components into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Footer from './components/Footer'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      
+      <Header/>
+      <Main/>
+      <Footer/>
     </>
   )
 }
