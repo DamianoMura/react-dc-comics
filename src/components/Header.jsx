8f4a91fd40c1a0da5d118990cd26ed32040195f3
DamianoMura <mura.damiano14@gmail.com>
2025-08-26 23:52:45 +0200

    [add] unstyled header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
 const Header = () => {
+
+
   return (
-    <div>Header</div>
+    <>
+    <div className="container">
+      <div className='logo'>
+        <img src="./img/dc-logo.png" alt="" />
+      </div>
+      <div className="menu">
+        <ul>
+          <li><a href="#">characters</a></li>
+          <li><a href="#">comics</a></li>
+          <li><a href="#">movies</a></li>
+          <li><a href="#">tv</a></li>
+          <li><a href="#">games</a></li>
+          <li><a href="#">collectables</a></li>
+          <li><a href="#">videos</a></li>
+          <li><a href="#">fans</a></li>
+          <li><a href="#">news</a></li>
+          <li><a href="#">shop</a></li>
+        </ul>
+      </div>
+    </div>
+    </>
+  
   )
 }
-
 export default Header
