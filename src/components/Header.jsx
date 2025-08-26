71ef56cd8548cc35f2a8ec1b2cc98097ede9b3af
DamianoMura <mura.damiano14@gmail.com>
2025-08-27 00:14:40 +0200

    [upd] header menu hardcoded

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,27 +4,30 @@ const Header = () => {
 
 
   return (
-    <>
-    <div className="container">
-      <div className='logo'>
-        <img src="./img/dc-logo.png" alt="" />
+    
+    <header>
+      <div className="container">
+        <div className='logo'>
+          <img src="./img/dc-logo.png" alt="" />
+        </div>
+        <div className="top-menu">
+          <ul>
+            <li><a href="#">characters</a></li>
+            <li className='active'><a href="#" >comics</a></li>
+            <li><a href="#">movies</a></li>
+            <li><a href="#">tv</a></li>
+            <li><a href="#">games</a></li>
+            <li><a href="#">collectables</a></li>
+            <li><a href="#">videos</a></li>
+            <li><a href="#">fans</a></li>
+            <li><a href="#">news</a></li>
+            <li><a href="#">shop</a></li>
+          </ul>
+        </div>
       </div>
-      <div className="menu">
-        <ul>
-          <li><a href="#">characters</a></li>
-          <li><a href="#">comics</a></li>
-          <li><a href="#">movies</a></li>
-          <li><a href="#">tv</a></li>
-          <li><a href="#">games</a></li>
-          <li><a href="#">collectables</a></li>
-          <li><a href="#">videos</a></li>
-          <li><a href="#">fans</a></li>
-          <li><a href="#">news</a></li>
-          <li><a href="#">shop</a></li>
-        </ul>
-      </div>
-    </div>
-    </>
+
+    </header>
+    
   
   )
 }
