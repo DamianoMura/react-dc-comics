737368a22682ca3f4dcfa7d61dd6df963fe894be
DamianoMura <mura.damiano14@gmail.com>
2025-08-27 09:48:35 +0200

    [upd] top-footer styling (to be finished)

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,38 +4,41 @@ const Footer = () => {
   return (
     <footer>
       <div className='top-footer'>
-        <ul>
-          <li>
-            <a href="#">
-              <img src="./img/buy-comics-digital-comics.png" alt="" />
-              <span>digital comics</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <img src="./img/buy-comics-merchandise.png" alt="" />
-              <span>merchandise</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <img src="./img/buy-comics-subscriptions.png" alt="" />
-              <span>subscription</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <img src="./img/buy-comics-shop-locator.png" alt="" />
-              <span>shop locator</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <img src="./img/buy-dc-power-visa.svg" alt="" />
-              <span>dc power visa</span>
-            </a>
-          </li>
-        </ul>
+        <div className="container">
+          <div className="row">
+            <div className="col-20">
+              <a href="#">
+                <img src="./img/buy-comics-digital-comics.png" alt="" />
+                <span>digital comics</span>
+              </a>
+            </div>
+            <div className="col-20">
+              <a href="#">
+                <img src="./img/buy-comics-merchandise.png" alt="" />
+                <span>merchandise</span>
+              </a>
+            </div>
+            <div className="col-20">
+              <a href="#">
+                <img src="./img/buy-comics-subscriptions.png" alt="" />
+                <span>subscription</span>
+              </a>
+            </div>
+            <div className="col-20">
+              <a href="#">
+                <img src="./img/buy-comics-shop-locator.png" alt="" />
+                <span>shop locator</span>
+              </a>
+            </div>
+            <div className="col-20">
+              <a href="#">
+                <img src="./img/buy-dc-power-visa.svg" alt="" />
+                <span>dc power visa</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        
       </div>
       <div className='mid-footer'></div>
       <div className='bottom-footer'></div>
