ac04f1d9cd44be26ddc219d3e1fc9a95e2ad30d5
DamianoMura <mura.damiano14@gmail.com>
2025-08-28 19:15:48 +0200

    [upd] bottom-footer content and styling

--- src/components/layout/BottomFooter.jsx
+++ src/components/layout/BottomFooter.jsx
@@ -2,11 +2,30 @@ import React from 'react'
 
 const BottomFooter = () => {
   return (
-    <div className='bottom-footer container'>
-      <div className="sign-up">
-        <button className="btn">SIGN-UP NOW!</button>
+    <div className='bottom-footer'>
+      <div className="flex-container-between align-center">
+        <div className="sign-up">
+          <button className="btn">SIGN-UP NOW!</button>
+        </div>
+        <div className="socials">
+          <span>FOLLOW US</span>
+          <a href="#">
+            <img src="./img/footer-facebook.png" alt="" />
+            </a>
+          <a href="#">
+            <img src="./img/footer-twitter.png" alt="" />
+            </a>
+          <a href="#">
+            <img src="./img/footer-youtube.png" alt="" />
+            </a>
+          <a href="#">
+            <img src="./img/footer-pinterest.png" alt="" />
+            </a>
+          <a href="#">
+            <img src="./img/footer-periscope.png" alt="" />
+            </a>
+          </div>
       </div>
-      <div className="socials">socials</div>
     </div>
   )
 }
