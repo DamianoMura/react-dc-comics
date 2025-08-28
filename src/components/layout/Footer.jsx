089bd2f38e07fb35975ae2c7e593ae7607077a28
DamianoMura <mura.damiano14@gmail.com>
2025-08-28 17:26:25 +0200

    [fix] top footer styling + separated in new file.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,43 +1,10 @@
-import React from 'react'
+import TopFooterLinks from "./TopFooterLinks"
 
 const Footer = () => {
   return (
     <footer>
       <div className='top-footer'>
-        <div className="container">
-          <div className="row">
-            <div className="col-20">
-              <a href="#">
-                <img src="./img/buy-comics-digital-comics.png" alt="" />
-                <span>digital comics</span>
-              </a>
-            </div>
-            <div className="col-20">
-              <a href="#">
-                <img src="./img/buy-comics-merchandise.png" alt="" />
-                <span>merchandise</span>
-              </a>
-            </div>
-            <div className="col-20">
-              <a href="#">
-                <img src="./img/buy-comics-subscriptions.png" alt="" />
-                <span>subscription</span>
-              </a>
-            </div>
-            <div className="col-20">
-              <a href="#">
-                <img src="./img/buy-comics-shop-locator.png" alt="" />
-                <span>shop locator</span>
-              </a>
-            </div>
-            <div className="col-20">
-              <a href="#">
-                <img src="./img/buy-dc-power-visa.svg" alt="" />
-                <span>dc power visa</span>
-              </a>
-            </div>
-          </div>
-        </div>
+          <TopFooterLinks/>
         
       </div>
       <div className='mid-footer'>
---
+++ src/components/layout/TopFooterLinks.jsx
@@ -0,0 +1,38 @@
+const TopFooterLinks = () => {
+  return (
+    <div className="list-icons">
+            <div className="link">
+              <a href="#">
+                <img src="./img/buy-comics-digital-comics.png" alt="" />
+                <span>digital comics</span>
+              </a>
+            </div>
+            <div className="link">
+              <a href="#">
+                <img src="./img/buy-comics-merchandise.png" alt="" />
+                <span>merchandise</span>
+              </a>
+            </div>
+            <div className="link">
+              <a href="#">
+                <img src="./img/buy-comics-subscriptions.png" alt="" />
+                <span>subscription</span>
+              </a>
+            </div>
+            <div className="link">
+              <a href="#">
+                <img src="./img/buy-comics-shop-locator.png" alt="" />
+                <span>shop locator</span>
+              </a>
+            </div>
+            <div className="link">
+              <a href="#">
+                <img src="./img/buy-dc-power-visa.svg" alt="" />
+                <span>dc power visa</span>
+              </a>
+            </div>
+        </div>
+  )
+}
+
+export default TopFooterLinks
