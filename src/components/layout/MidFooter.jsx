fdf029a5fb7413cf605e914a8815030e4f53ea6e
DamianoMura <mura.damiano14@gmail.com>
2025-08-28 18:33:53 +0200

    [upd] better styled mid footer

--- src/components/layout/MidFooter.jsx
+++ src/components/layout/MidFooter.jsx
@@ -2,40 +2,37 @@ import React from 'react'
 
 const MidFooter = () => {
   return (
-    <div className='mid-footer'>
-        <div className="container-sm">
-          <div className="row">
-            <div className="col-50"><div className="container">
+        <div className='mid-footer'>
+      <div className="container-sm">
+        <div className="row">
+          <div className="col-50">
+            <div className="container">
               <div className="row">
                 <div className="col-33">
                   <div className="foot-links">
-                   <h3>DC COMICS</h3>
-                   <ul>
-                    
-                    <li><a href="#">Characters</a></li>
-                    <li><a href="#">Comics</a></li>
-                    <li><a href="#">Movies</a></li>
-                    <li><a href="#">TV</a></li>
-                    <li><a href="#">Games</a></li>
-                    <li><a href="#">Videos</a></li>
-                    <li><a href="#">News</a></li>
-                   
-                  </ul>
-                  </div>
-                  <div className="foot-links pb-80">
-                    <h3>SHOP</h3>
+                    <h4>DC COMICS</h4>
                     <ul>
-                    
+                      <li><a href="#">Characters</a></li>
+                      <li><a href="#">Comics</a></li>
+                      <li><a href="#">Movies</a></li>
+                      <li><a href="#">TV</a></li>
+                      <li><a href="#">Games</a></li>
+                      <li><a href="#">Videos</a></li>
+                      <li><a href="#">News</a></li>
+                    </ul>
+                  </div>
+                  <div className="foot-links">
+                    <h4>SHOP</h4>
+                    <ul>
                       <li><a href="#"></a>Shop DC</li>
                       <li><a href="#"></a>Shop Dc Collectibles</li>
                     </ul>
                   </div>
                 </div>
                 <div className="col-33">
-                  <div className="foot-links pb-80">
-                    <h3>DC</h3>
+                  <div className="foot-links">
+                    <h4>DC</h4>
                     <ul>
-                    
                       <li><a href="#"></a>Terms of Use</li>
                       <li><a href="#"></a>Privacy Policy (new) </li>
                       <li><a href="#"></a>Ad Choices</li>
@@ -51,10 +48,9 @@ const MidFooter = () => {
                   </div>
                 </div>
                 <div className="col-33">
-                  <div className="foot-links pb-80">
-                    <h3>SITES</h3>
+                  <div className="foot-links">
+                    <h4>SITES</h4>
                     <ul>
-                    
                       <li><a href="#">DC</a></li>
                       <li><a href="#">MAD Magazine</a></li>
                       <li><a href="#">DC Kids</a></li>
@@ -64,12 +60,16 @@ const MidFooter = () => {
                   </div>
                 </div>
               </div>
-            </div></div>
-            <div className="col-50">
-              <img src="./img/dc-logo-bg.png" alt="" />
             </div>
           </div>
-        </div>
+          <div className="col-50 dc-bg">
+
+          </div>
+      </div>
+      </div>
+      
+    
+        
       </div>
   )
 }
