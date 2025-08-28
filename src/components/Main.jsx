ea0290d5499659fe351ac19e5b08002ee162e265
DamianoMura <mura.damiano14@gmail.com>
2025-08-28 21:00:10 +0200

    [add] ComicsList component + styled

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ComicsList from './ComicsList'
+const Main = () => {
+  return (
+    <main>
+      <div className="container">
+        <div className="row">
+          <div className="col-100">
+            <ComicsList/>
+          </div>
+        </div>
+      </div>
+      
+    </main>
+  )
+}
+
+export default Main
